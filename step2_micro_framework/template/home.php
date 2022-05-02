dced0cd891c926e28361dfe47ccb08cfd8185b30
jean viven <jeanvivien.sicot@lapiscine.pro>
2022-05-02 11:51:12 +0200

    maj

--- step2_micro_framework/template/home.php
+++ step2_micro_framework/template/home.php
@@ -1,7 +1,7 @@
 <nav class="nav">
-    <a class="nav-link active" aria-current="page" href="http://localhost:8000/?page=home">Home</a>
-    <a class="nav-link" href="http://localhost:8000/?page=link">Link</a>
-    <a class="nav-link" href="http://localhost:8000/?page=about">About</a>
+    <a class="nav-link active" href="/?page=home">Home</a>
+    <a class="nav-link" href="/?page=link">Link</a>
+    <a class="nav-link" href="/?page=about">About</a>
 </nav>
 
 <H1>HELLO WORLD</H1>
--- step3_micro_framework/template/template_part/_navbar.php
+++ step3_micro_framework/template/template_part/_navbar.php
@@ -1,5 +1,5 @@
 <nav class="nav">
-    <a class="nav-link active" aria-current="page" href="http://localhost:8000/?page=home">Home</a>
-    <a class="nav-link" href="http://localhost:8000/?page=article">Article</a>
-    <a class="nav-link" href="http://localhost:8000/?page=link">Link</a>
+    <a class="nav-link active" aria-current="page" href="/?page=home">Home</a>
+    <a class="nav-link" href="/?page=article">Article</a>
+    <a class="nav-link" href="/?page=link">Link</a>
 </nav>
